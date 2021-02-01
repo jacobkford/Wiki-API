047e60c862dac07f4c8f8b1fa494bb8817eec8a5
Jay <68663887+jacobkford@users.noreply.github.com>
2021-02-01 23:05:15 +0000

    init

--- tests/users/users.model.test.js
+++ tests/users/users.model.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
-
 const mongoose = require('mongoose');
-const User = require("../models/user");
+const User = require("../../models/user");
 const userData = { email: "test@testmail.com", password: "Testy123" };
 
 describe('User Model Test', () => {
@@ -20,7 +19,7 @@ describe('User Model Test', () => {
         });
     });
 
-    it('create & save user successfully', async () => {
+    it('should create and save user successfully', async () => {
         const validUser = new User(userData);
         const savedUser = await validUser.save();
         // Object Id should be defined when successfully saved to MongoDB.
@@ -31,26 +30,41 @@ describe('User Model Test', () => {
 
     // Test Schema is working!!!
     // You shouldn't be able to add in any field that isn't defined in the schema
-    it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
-        const userWithInvalidField = new User({ email: 'newuser', password: 'password' });
+    it('insert user successfully, but if one or more fields that are not defined in schema and should return undefined', async () => {
+        const userWithInvalidField = new User({ email: 'newuser@jmail.com', password: 'password', admin: 'true' });
         const savedUserWithInvalidField = await userWithInvalidField.save();
         expect(savedUserWithInvalidField._id).toBeDefined();
-        expect(savedUserWithInvalidField.email).toBeUndefined();
+        expect(savedUserWithInvalidField.admin).toBeUndefined();
     });
 
     // Test Validation is working!!!
     // It should us told us the errors in on gender field.
-    it('create user without required field should failed', async () => {
+    it('should fail creating user without required field', async () => {
         const userWithoutRequiredField = new User({ email: 'newuser@jmail.net' });
         let err;
         try {
             const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
             error = savedUserWithoutRequiredField;
         } catch (error) {
-            err = error
+            err = error;
         }
         expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
         expect(err.errors.password).toBeDefined();
+    });
+
+    it('creating two users with the same email should fail', async () => {
+        const firstUserWithSameEmail = new User({ email: "notunique@fail.com", password: "Thisshouldfail123!" });
+        const secondUserWithSameEmail = new User({ email: "notunique@fail.com", password: "Hopefullythisfails1?" });
+        let err;
+        try {
+            firstUserWithSameEmail.save();
+            const secondUser = await secondUserWithSameEmail.save();
+            error = secondUser;
+        } catch (error) {
+            err = error;
+        }
+        expect(err).toBeInstanceOf(mongoose.mongo.MongoError);
+        expect(err.keyValue.email).toBeDefined();
     });
 
     // Cleans up database between each test
@@ -58,8 +72,13 @@ describe('User Model Test', () => {
         await User.deleteMany()
     });
 
-    afterAll(async () => {
+    afterAll(async (done) => {
         // Removes the User collection
-        await User.drop()
+        try {
+            await mongoose.connection.db.dropDatabase(done)
+            await mongoose.connection.close(done);
+        } catch (err) {
+            console.log(err);
+        }
     });
 });
