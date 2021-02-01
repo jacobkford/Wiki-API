ccc6dbf7793eea61639fe6bc6b6d5e993394d4a2
Jay <68663887+jacobkford@users.noreply.github.com>
2021-02-01 21:26:55 +0000

    init

---
+++ tests/app.test.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+
+const mongoose = require('mongoose');
+const User = require("../models/user");
+const userData = { email: "test@testmail.com", password: "Testy123" };
+
+describe('User Model Test', () => {
+    // It's just so easy to connect to the MongoDB Memory Server 
+    // By using mongoose.connect
+    beforeAll(async () => {
+        await mongoose.connect(process.env.DB, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1)
+            }
+        });
+    });
+
+    it('create & save user successfully', async () => {
+        const validUser = new User(userData);
+        const savedUser = await validUser.save();
+        // Object Id should be defined when successfully saved to MongoDB.
+        expect(savedUser._id).toBeDefined();
+        expect(savedUser.email).toBe(userData.email);
+        expect(savedUser.password).toBe(userData.password); 
+    });
+
+    // Test Schema is working!!!
+    // You shouldn't be able to add in any field that isn't defined in the schema
+    it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
+        const userWithInvalidField = new User({ email: 'newuser', password: 'password' });
+        const savedUserWithInvalidField = await userWithInvalidField.save();
+        expect(savedUserWithInvalidField._id).toBeDefined();
+        expect(savedUserWithInvalidField.email).toBeUndefined();
+    });
+
+    // Test Validation is working!!!
+    // It should us told us the errors in on gender field.
+    it('create user without required field should failed', async () => {
+        const userWithoutRequiredField = new User({ email: 'newuser@jmail.net' });
+        let err;
+        try {
+            const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
+            error = savedUserWithoutRequiredField;
+        } catch (error) {
+            err = error
+        }
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+        expect(err.errors.password).toBeDefined();
+    });
+
+    // Cleans up database between each test
+    afterEach(async () => {
+        await User.deleteMany()
+    });
+
+    afterAll(async () => {
+        // Removes the User collection
+        await User.drop()
+    });
+});
